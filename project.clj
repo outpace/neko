bbd171aea72c453155c72e05b16ac59ad0a15d33
Alexander Yakushev <alex@bytopia.org>
2014-12-29 09:31:40 +0200

    Version 3.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject neko "3.1.0-SNAPSHOT"
+(defproject neko "3.1.0"
   :description "Neko is a toolkit designed to make Android development using Clojure easier and more fun."
   :url "https://github.com/clojure-android/neko"
   :license {:name "Eclipse Public License"
