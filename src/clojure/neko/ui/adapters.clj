380631c4a0acdfdf557c96f543919e93815acc34
Alexander Yakushev <alex@bytopia.org>
2015-02-03 18:49:29 +0200

    [n.u.adapters] Refactor ref-adapter to use safe-for-ui in callbacks

--- src/clojure/neko/ui/adapters.clj
+++ src/clojure/neko/ui/adapters.clj
@@ -1,9 +1,10 @@
 (ns neko.ui.adapters
   "Contains custom adapters for ListView and Spinner."
-  (:use [neko.threading :only [on-ui]]
-        [neko.ui :only [make-ui-element]])
-  (:import neko.ui.adapters.InterchangeableListAdapter
-           android.view.View))
+  (:require [neko.debug :refer [safe-for-ui]]
+            [neko.threading :refer [on-ui]]
+            [neko.ui :refer [make-ui-element]])
+  (:import android.view.View
+           neko.ui.adapters.InterchangeableListAdapter))
 
 (defn ref-adapter
   "Takes a function that creates a View, a function that updates a
@@ -17,21 +18,26 @@
   ref-type. `access-fn` argument is optional, it is called on the
   value of ref-type to get the list to be displayed."
   ([create-view-fn update-view-fn ref-type]
-     (ref-adapter create-view-fn update-view-fn ref-type identity))
+   (ref-adapter create-view-fn update-view-fn ref-type identity))
   ([create-view-fn update-view-fn ref-type access-fn]
-     {:pre [(fn? create-view-fn) (fn? update-view-fn)
-            (instance? clojure.lang.IFn access-fn)
-            (instance? clojure.lang.IDeref ref-type)]}
-     (let [create-fn (fn [context]
-                       (let [view (create-view-fn)]
-                         (if (instance? View view)
-                           view
-                           (make-ui-element
-                            context view
-                            {:container-type :abs-listview-layout}))))
-           adapter (InterchangeableListAdapter. create-fn update-view-fn
-                                                (access-fn @ref-type))]
-       (add-watch ref-type ::adapter-watch
-                  (fn [_ __ ___ new-state]
-                    (on-ui (.setData adapter (access-fn new-state)))))
-       adapter)))
+   {:pre [(fn? create-view-fn) (fn? update-view-fn)
+          (instance? clojure.lang.IFn access-fn)
+          (instance? clojure.lang.IDeref ref-type)]}
+   (let [create-fn (fn [context]
+                     (or (safe-for-ui
+                          (let [view (create-view-fn)]
+                            (if (instance? View view)
+                              view
+                              (make-ui-element
+                               context view
+                               {:container-type :abs-listview-layout}))))
+                         (android.view.View. context)))
+         adapter (InterchangeableListAdapter.
+                  create-fn
+                  (fn [pos view parent data]
+                    (safe-for-ui (update-view-fn pos view parent data)))
+                  (access-fn @ref-type))]
+     (add-watch ref-type ::adapter-watch
+                (fn [_ __ ___ new-state]
+                  (on-ui (.setData adapter (access-fn new-state)))))
+     adapter)))
