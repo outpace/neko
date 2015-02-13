eb35fbc1e8e96fa921e2e1f35fb9ddd8fc5f8066
Alexander Yakushev <alex@bytopia.org>
2015-02-13 23:23:12 +0200

    [n.u.adapters] Make cursor-adapter take cursor-fn and add means of updating

--- src/clojure/neko/ui/adapters.clj
+++ src/clojure/neko/ui/adapters.clj
@@ -5,6 +5,7 @@
             [neko.threading :refer [on-ui]]
             [neko.ui :refer [make-ui-element]])
   (:import android.view.View
+           neko.data.sqlite.TaggedCursor
            [neko.ui.adapters InterchangeableListAdapter TaggedCursorAdapter]))
 
 (defn ref-adapter
@@ -13,7 +14,7 @@
   data. Returns an Adapter object that displays ref-type contents.
   When ref-type is updated, Adapter gets updated as well.
 
-  `create-view-fn` is a function of no arguments. `update-view-fn` is
+  `create-view-fn` is a nullary function. `update-view-fn` is
   a function of four arguments: element position, view to update,
   parent view container and the respective data element from the
   ref-type. `access-fn` argument is optional, it is called on the
@@ -26,7 +27,7 @@
           (instance? clojure.lang.IDeref ref-type)]}
    (let [create-fn (fn [context]
                      (or (safe-for-ui
-                          (let [view (create-view-fn)]
+                          (let [view (create-view-fn context)]
                             (if (instance? View view)
                               view
                               (make-ui-element
@@ -44,18 +45,25 @@
      adapter)))
 
 (defn cursor-adapter
-  "Takes a context, TaggedCursor instance, a function that creates a View, and a
-  function that updates a view according to the element. Returns an Adapter
-  object that displays cursor contents.
+  "Takes a context, a function that creates a View, and a function that updates
+  a view according to the element, and a TaggedCursor instance or
+  cursor-producing function. Returns an Adapter object that displays cursor
+  contents.
 
-  `create-view-fn` is a function of Context, that returns a UI tree or a View.
+  `create-view-fn` is a nullary function that returns a UI tree or a View.
   `update-view-fn` is a function of three arguments: view to update, cursor, and
-  data extracted from the cursor."
-  [context cursor create-view-fn update-view-fn]
-  {:pre [(fn? create-view-fn) (fn? update-view-fn)]}
+  data extracted from the cursor. `cursor-or-cursor-fn` can be a nullary
+  function that returns a TaggedCursor cursor object when called, or just a
+  cursor. In the former case you can refresh adapter by calling `(.updateCursor
+  adapter)`, in the latter you have to call `(.updateCursor adapter
+  new-cursor)`."
+  [context create-view-fn update-view-fn cursor-or-cursor-fn]
+  {:pre [(fn? create-view-fn) (fn? update-view-fn)
+         (or (fn? cursor-or-cursor-fn)
+             (instance? TaggedCursor cursor-or-cursor-fn))]}
   (let [create-fn (fn [context]
                     (or (safe-for-ui
-                         (let [view (create-view-fn context)]
+                         (let [view (create-view-fn)]
                            (if (instance? View view)
                              view
                              (make-ui-element
@@ -63,7 +71,15 @@
                               {:container-type :abs-listview-layout}))))
                         (android.view.View. context)))]
     (TaggedCursorAdapter.
-     context cursor create-fn
+     context create-fn
      (fn [view cursor data]
        (safe-for-ui (update-view-fn view cursor data)))
-     entity-from-cursor)))
+     cursor-or-cursor-fn)))
+
+(defn update-cursor
+  "Updates cursor in a given TaggedCursorAdapter. Second argument is necessary
+  if the adapter was created with a cursor rather than cursor-fn."
+  ([^TaggedCursorAdapter cursor-adapter]
+   (.updateCursor cursor-adapter))
+  ([^TaggedCursorAdapter cursor-adapter new-cursor]
+   (.updateCursor cursor-adapter new-cursor)))
--- src/java/neko/ui/adapters/TaggedCursorAdapter.java
+++ src/java/neko/ui/adapters/TaggedCursorAdapter.java
@@ -6,20 +6,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import clojure.lang.IFn;
+import clojure.lang.RT;
 import neko.data.sqlite.TaggedCursor;
 
 public class TaggedCursorAdapter extends CursorAdapter {
 
     private IFn createViewFn;
     private IFn updateViewFn;
-    private IFn getDataFn;
+    private IFn updateCursorFn;
+    private IFn entityFromCursor;
 
-    public TaggedCursorAdapter(Context context, TaggedCursor cursor,
-                               IFn createViewFn, IFn updateViewFn, IFn getDataFn) {
-        super(context, cursor);
+    public TaggedCursorAdapter(Context context, IFn createViewFn,
+                               IFn updateViewFn, Object cursorOrFn) {
+        super(context, null);
+        Cursor cursor;
         this.createViewFn = createViewFn;
         this.updateViewFn = updateViewFn;
-        this.getDataFn = getDataFn;
+        this.entityFromCursor = (IFn)RT.var("neko.data.sqlite", "entity-from-cursor").deref();
+        if (cursorOrFn instanceof TaggedCursor)
+            updateCursor((TaggedCursor)cursorOrFn);
+        else {
+            this.updateCursorFn = (IFn)cursorOrFn;
+            updateCursor();
+        }
     }
 
     @Override
@@ -29,11 +38,22 @@ public class TaggedCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        updateViewFn.invoke(view, cursor, getDataFn.invoke(cursor));
+        updateViewFn.invoke(view, cursor, entityFromCursor.invoke(cursor));
     }
 
     public Object getItem(int position) {
-        return getDataFn.invoke(super.getItem(position));
+        return entityFromCursor.invoke(super.getItem(position));
+    }
+
+    public void updateCursor() {
+        if (updateCursorFn == null)
+            throw new RuntimeException("Zero-argument updateCursor() needs adapter to be created with cursor-fn");
+        updateCursor((TaggedCursor)updateCursorFn.invoke());
+    }
+
+    public void updateCursor(TaggedCursor cursor) {
+        changeCursor(cursor);
+        notifyDataSetChanged();
     }
 
 }
